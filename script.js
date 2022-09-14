f178d71fb2da9c4281701c3c4073a8ba50560c58
Pablo Dominguez <pasaxo@gmail.com>
2022-09-14 10:12:58 -0300

    requisito 6

--- script.js
+++ script.js
@@ -10,6 +10,35 @@ function generateColor() {
 
 const positionBall = document.getElementsByClassName('ball');
 
+const color = [];
+
 for (let index = 0; index < positionBall.length; index += 1) {
   positionBall[index].style.backgroundColor = generateColor();
+  color.push(positionBall[index].style.backgroundColor);
 }
+
+const randomColor = Math. floor(Math. random() * color.length);
+
+const printColor = document.getElementById('rgb-color').innerText = color[randomColor];
+
+function checkColor(event) {
+  const result = document.getElementById('answer');
+
+  if (event.target.style.backgroundColor === printColor) {
+    result.innerText = 'Acertou!';
+  } else {
+    result.innerText = 'Errou! Tente novamente!';
+  }
+}
+
+for (let index = 0; index < positionBall.length; index += 1) {
+  const positionColorBall = document.getElementsByClassName('ball')[index];
+  positionColorBall.addEventListener('click', checkColor);
+}
+
+const positionButton = document.getElementById('reset-game');
+
+function reset() {
+  location.reload(true);
+}
+positionButton.addEventListener('click', reset);
